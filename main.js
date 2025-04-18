84ab8d7606ad8d4a4bf5c4a5b84c43512dab1aa1
John Daniels <john@theelectricengine.com>
2025-04-18 19:33:44 +0100

    All of the basic app functionality, still needs some fiddling with display

--- main.js
+++ main.js
@@ -9,9 +9,31 @@ function hidePage(id) {
 }
 
 
-function submitPostCode() {
+async function submitPostCode() {
   showPage("loading");
-  alert('beep doop, getting postcode data');
+  const postcode = this.previousElementSibling.value
+  const url = encodeURI('https://workers-playground-dark-field-e4ba.john-521.workers.dev/?postcode=' + postcode);
+  const select = document.getElementById("address_results");
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+
+    while (select.firstChild) {
+      select.removeChild(select.firstChild);
+    }
+
+    for (const address of json){
+      select.insertAdjacentHTML("beforeend","<option value='" + address.value + "'>" + address.label + "</option>");
+      select.parentElement.classList.remove("is-loading");
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+
   hidePage("loading");
   hidePage("postcode");
   showPage("pickhouse");
@@ -20,23 +42,103 @@ let postcodebuttons=document.getElementsByClassName("postcodebutton");
 for (const postcodebutton of postcodebuttons) {
   postcodebutton.onclick = submitPostCode;
 }
+let postcodefields=document.querySelectorAll('[placeholder="Postcode"]');
+for (const postcodefield of postcodefields) {
+  postcodefield.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      this.nextElementSibling.click();
+    }
+  });
+}
 
 function resetUPRN() {
-  alert('beep boop what uprn?');
+  localStorage.removeItem('uprn');
+  window.location.reload();
 }
 let resetbuttons=document.getElementsByClassName("resetbutton");
 for (const resetbutton of resetbuttons) {
   resetbutton.onclick = resetUPRN;
 }
 
-function getBinData() {
+async function getBinDataFromID() {
   showPage("loading");
-  alert('beep boop, getting bin data');
+  const houseid = document.getElementById("address_results").value;
+  const url = "https://super-dream-e5c7.john-521.workers.dev/?houseid=" + houseid;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    const uprn = json["property-UPRN"];
+    localStorage.setItem("uprn", uprn);
+    getBinData(uprn);
+  } catch (error) {
+    console.error(error.message);
+  }
+
   hidePage("loading");
   hidePage("pickhouse");
   showPage("showbins");
 }
-document.getElementById("addresselectbutton").onclick = getBinData;
+document.getElementById("addresselectbutton").onclick = getBinDataFromID;
+
+async function getBinData(uprn) {
+  const url = "https://workers-playground-winter-silence-5b90.john-521.workers.dev/?uprn=" + uprn;
+  const binDataHolder = document.getElementById("bindata");
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    const binData = json["tab_collections"];
+    console.log(binData);
+
+    for (const bin of binData) {
+      let binColour = "";
+      switch (bin.colour) {
+        case "Grey":
+          binColour = "has-background-grey-light";
+          break;
+        case "Brown":
+          binColour = "has-background-warning-dark";
+          break;
+        case "Blue":
+          binColour = "has-background-link";
+          break;
+        case "Green":
+          binColour = "has-background-success";
+          break;
+      }
+      const binHTML = `<article class="media">
+  <figure class="media-left ${binColour}">
+    <p class="image is-64x64">
+      <img src="bin.svg" />
+    </p>
+  </figure>
+  <div class="media-content">
+    <div class="content">
+      <p>
+        ${bin.date}
+        <br />
+        ${bin.type}
+      </p>
+    </div>
+  </div>
+</article>`
+      binDataHolder.insertAdjacentHTML("beforeend",binHTML);
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+  hidePage("loading");
+
+}
+
 
 function getStarted() {
   hidePage("start");
@@ -45,16 +147,15 @@ function getStarted() {
 document.getElementById("getstarted").onclick = getStarted;
 
 
-let uprn=0;
+const uprn = localStorage.getItem("uprn");
 
 function welcomePage(uprn) {
-  if (uprn == 0) {
+  if (uprn) {
+    getBinData(uprn);
+    showPage("showbins");
+  } else {
     hidePage("loading");
     showPage("start");
-  } else {
-    alert("getting bin data");
-    hidePage("loading");
-    showPage("showbins");
   }
 }
 
